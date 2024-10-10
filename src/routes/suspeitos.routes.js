6b349c081670f3d05de68dddec39cfd7361f4ceb
s0uzx <sarasouza.tds2.senai@gmail.com>
2024-10-10 17:12:53 -0300

    feat:craete post route

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -18,6 +18,41 @@ suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).send({ suspeitos });
 });
 
+//Rota para criar emoções
+suspeitosRoutes.post("/", (req, res) => {
+    const { nome, profissão, envolvimento, nivelSus } = req.body;
+
+     //validação dos campos nome e partido
+     if (!nome || !profissão){
+        return res.status(400).send({ error: "O nome e a profissão do suspeito são obrigatórios."})
+     }
+
+     //validação de idade
+     if (nivelSus != "Baixo" && nivelSus != "Médio" && nivelSus != "Alto") {
+        return res.status(400).send({ message: "O nível de suspeita não coincide com as opções."})
+     }
+
+     if (envolvimento != "Sim" && envolvimento != "Não") {
+        return res.status(400).send({ message: "Não foi possível constatar o envolvimento do suspeito."})
+     }
+
+    const novoSuspeito = {
+        id: Math.floor(Math.random() * 1000000),
+        nome,
+        profissão,
+        envolvimento,
+        nivelSus,
+    };
+
+    suspeitos.push(novoSuspeito);
+   
+    return res.status(201)
+    .json({ 
+        mensagem: "Suspeito adicionado com sucesso!",
+        novoSuspeito,
+     });
+    
+});
 
 
 export default suspeitosRoutes;
