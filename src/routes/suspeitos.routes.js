cc7113d8cd0ca2437832d3db392f4211267e27a5
s0uzx <sarasouza.tds2.senai@gmail.com>
2024-10-17 08:46:20 -0300

    feat: create get by id method

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -1,4 +1,4 @@
-import {Router}from "express";
+import { Router } from "express";
 
 const suspeitosRoutes = Router();
 
@@ -6,11 +6,11 @@ let suspeitos = [
     {
         id: Math.floor(Math.random() * 1000000),
         nome: 'Neymar Junior',
-        profissão: 'Atleta',
+        profissao: 'Atleta',
         envolvimento: "Sim", // envolvimento em apostas
         nivelSus: "Alto", // nível de suspeita
     },
-   
+
 ]
 
 //Rota para buscar todas os suspeitos
@@ -18,42 +18,59 @@ suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).send({ suspeitos });
 });
 
-//Rota para criar emoções
+//Rota para criar suspeitos
 suspeitosRoutes.post("/", (req, res) => {
-    const { nome, profissão, envolvimento, nivelSus } = req.body;
+    const { nome, profissao, envolvimento, nivelSus } = req.body;
 
-     //validação dos campos nome e partido
-     if (!nome || !profissão){
-        return res.status(400).send({ error: "O nome e a profissão do suspeito são obrigatórios."})
-     }
+    //validação dos campos nome e profissao
+    if (!nome || !profissao) {
+        return res.status(400).send({ error: "O nome e a profissão do suspeito são obrigatórios." })
+    }
 
-     //validação de idade
-     if (nivelSus != "Baixo" && nivelSus != "Médio" && nivelSus != "Alto") {
-        return res.status(400).send({ message: "O nível de suspeita não coincide com as opções."})
-     }
+    //validação de idade
+    if (nivelSus != "Baixo" && nivelSus != "Médio" && nivelSus != "Alto") {
+        return res.status(400).send({ message: "O nível de suspeita não coincide com as opções." })
+    }
 
-     if (envolvimento != "Sim" && envolvimento != "Não") {
-        return res.status(400).send({ message: "Não foi possível constatar o envolvimento do suspeito."})
-     }
+    if (envolvimento != "Sim" && envolvimento != "Não") {
+        return res.status(400).send({ message: "Não foi possível constatar o envolvimento do suspeito." })
+    }
 
     const novoSuspeito = {
         id: Math.floor(Math.random() * 1000000),
         nome,
-        profissão,
+        profissao,
         envolvimento,
         nivelSus,
     };
 
     suspeitos.push(novoSuspeito);
-   
+
     return res.status(201)
-    .json({ 
-        mensagem: "Suspeito adicionado com sucesso!",
-        novoSuspeito,
-     });
-    
+        .json({
+            mensagem: "Suspeito adicionado com sucesso!",
+            novoSuspeito,
+        });
+
 });
 
+
+//Rota para buscar pelo id
+suspeitosRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+
+    //console.log(id);
+    const suspeito = suspeitos.find((suspect) => suspect.id == id);
+
+
+    if (!suspeito) {
+        return res.status(404).send({ message: "Suspeito não encontrado" });
+    }
+
+
+    return res.status(200).send({ message: "Suspeito encontrado", suspeito });
+});
 
 export default suspeitosRoutes;
 
